116985845b72edcc0b084d1de1ea4c1f7f3a6dc0
Mohit kumar <mohitsidhu157@gmail.com>
2019-10-27 19:21:39 +0530

    completed

---
+++ src/containers/Courses/Courses.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Courses.css';
+import { Route } from "react-router-dom";
+import Course from '../Course/Course';
+
+import { Link } from "react-router-dom";
+
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        
+                        this.state.courses.map( course => {
+                            console.log(this.props.match.url);
+                            return (
+                                <Link 
+                                    to={{
+                                        pathname : this.props.match.url+"/"+ course.id,
+                                        search : "?title=" + course.title
+                                    }}
+                                    key={course.id}
+                                >
+                                    <article className="Course">{course.title}</article>
+                                </Link>
+                            );
+                        } )
+                    }
+                </section>
+                <Route path={this.props.match.url + "/:id"} component={Course} />
+                
+            </div>
+        );
+    }
+}
+
+export default Courses;
